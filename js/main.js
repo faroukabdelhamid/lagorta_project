37ff0d0c315b45c256a4bb7419e86c3dcc996420
faroukabdelhamid222 <farouk.abdelhamid22@gmail.com>
2020-03-04 23:06:54 +0200

    lagorta

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+$(".f-im-col").click(function() { $(".col-im-f").toggle(0); $("#img img").not(".col-im-f").hide(0);});
+
+$(".s-im-col").click(function() {  $(".col-im-s") .toggle(0) ;$("#img img").not(".col-im-s").hide(0); });
+
+$(".th-im-col").click(function() {  $(".col-im-th") .toggle(0);$("#img img").not(".col-im-th").hide(0);  });
+$(".fo-im-col").click(function() {  $(".col-im-fo") .toggle(0) ;$("#img img").not(".col-im-fo").hide(0); });
+$(".fi-im-col").click(function() {  $(".col-im-fi") .toggle(0) ;$("#img img").not(".col-im-fi").hide(0); });
+$(".sex-im-col").click(function() {  $(".col-im-sex") .toggle(0) ;$("#img img").not(".col-im-sex").hide(0); });
+$(".sev-im-col").click(function() {  $(".col-im-sev") .toggle(0) ;$("#img img").not(".col-im-sev").hide(0); });
+
+
+
+
+
+
+
+
+
+ا
